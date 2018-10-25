d896f67cd57c04dda5aa326ee33721a1ef33754d
adam <adam.belniak@balticdatascience.com>
2018-10-25 11:40:48 +0200

    add helper script for retrieve training data set from json

--- helper.py
+++ helper.py
@@ -1,23 +1,18 @@
 import os
-import shutil
-
 import imutils as imutils
-import scipy.misc
-
-import helper_batch
 import json
 import urllib.request
 import cv2
 import numpy as np
-import shutil
-from sklearn import datasets, linear_model
-from sklearn.model_selection import train_test_split
-#
-# def prepare_data(dir, data=[]):
-#     file_names = os.listdir(dir)
-#     for file in file_names:
-#
-#     return data
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-m", "--mode", help="choose for which model, data will be provided", action="store", dest="mode")
+parser.add_argument("-f", "--file", help="file name of json with training data ", action="store", dest="file")
+
+OUTPUT_DIRECTORY = "training_set_500"
+
 
 def prepare_data(dir, data):
     file_names = os.listdir(dir)
@@ -28,21 +23,19 @@ def prepare_data(dir, data):
             data.append(res)
     return data
 
+
 def retrieve_file(single_data):
-    dir = 'training_set_500/' + single_data['External ID']
+    dir = os.path.join(OUTPUT_DIRECTORY, single_data['External ID'])
     os.mkdir(dir)
     f = open(dir + '/origin.jpg', 'wb')
     f.write(urllib.request.urlopen(single_data['Labeled Data']).read())
     f.close()
     for mask in single_data['Masks']:
-        # f = open(dir + '/' + mask + '.jpg', 'wb')
         resp = urllib.request.urlopen(single_data['Masks'][mask])
         image = np.asarray(bytearray(resp.read()), dtype="uint8")
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         rotated = imutils.rotate_bound(image, 90)
         cv2.imwrite(dir + '/' + mask + '.jpg', rotated)
-        # f.write(image)
-        # f.close()
 
 
 def retriev_classify_set(single_data, dir):
@@ -55,33 +48,39 @@ def retriev_classify_set(single_data, dir):
 
 
 if __name__ == '__main__':
-    # gen = helper_batch.gen_batch_function('data_road/training', (160, 576))
-    dir = 'sharpness_set'
-    with open('sharpness_set.json') as json_data:
-        try:
-            os.mkdir(dir)
-            os.mkdir(os.path.join(dir, 'True'))
-            os.mkdir(os.path.join(dir, 'False'))
-        except FileExistsError as e:
-            print("Directory Exist")
-            pass
+    args = parser.parse_args()
 
-        d = json.load(json_data)
-        for data in d:
-            try:
-                retriev_classify_set(data, dir)
-            except Exception as e:
-                print(e)
-                pass
+    if args.file is None:
+        print("please provide file name with images names")
+    else:
+        filename = args.file
+        if args.mode and args.mode == 'classifier':
+            dir = 'sharpness_set'
+            with open(filename) as json_data:
+                try:
+                    os.mkdir(dir)
+                    os.mkdir(os.path.join(dir, 'True'))
+                    os.mkdir(os.path.join(dir, 'False'))
+                except FileExistsError as e:
+                    print("Directory Exist")
+                    pass
 
-
-
-    # image = imutils.rotate_bound(scipy.misc.imread('training_set_2/IMG_1069.JPG/origin.jpg'), 90)
-    # cv2.imshow('image', image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    #
-    # for i, (x, y) in enumerate(gen(1)):
-    #     if i == 1:
-    #         print ("a")
-
+                d = json.load(json_data)
+                for data in d:
+                    try:
+                        retriev_classify_set(data, dir)
+                    except Exception as e:
+                        print(e)
+                        pass
+        if args.mode and args.mode == 'segmentation':
+            with open(filename) as json_data:
+                try:
+                    os.mkdir(OUTPUT_DIRECTORY)
+                except:
+                    pass
+                d = json.load(json_data)
+                for data in d:
+                    try:
+                        retrieve_file(data)
+                    except:
+                        pass
